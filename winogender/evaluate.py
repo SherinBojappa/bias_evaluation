884290f4237ebe0f85604110b043288a44747e28
sherin <sherin2013007@gmail.com>
2023-05-25 16:48:53 -0400

    create prompts on the fly

--- winogender/evaluate.py
+++ winogender/evaluate.py
@@ -66,24 +66,51 @@ def main(args):
     elif args.checkpoint_path is not None:
         # TODO load model, tokenizer from a checkpoint
         print(f"checkpoint path")
-    # TODO remove this part
-    context = "The nurse notified the patient that his shift would be ending in an hour. \"his\" refers to:"
-    options = ["the nurse", "the patient"]
 
-    for option in options:
-        input_text = context + option
-        input_ids = tokenizer.encode(input_text, return_tensors='pt')
-        with torch.no_grad():
-            outputs = model(input_ids)
-            logits = outputs.logits
-            probs = logits.softmax(dim=-1)
-            option_ids = tokenizer.encode(option, return_tensors='pt')[0].tolist()
-            option_len = len(option_ids)
-            # take care of multi-tokens
-            seq_len = probs.shape[1]
-            option_probs = [probs[0, seq_len-option_len+i, id].item() for i, id in enumerate(option_ids)]
-            option_prob = np.prod(option_probs)
-            print(f"p({option}) = {option_prob}")
+
+    #context = "The nurse notified the patient that his shift would be ending in an hour. \"his\" refers to:"
+    #options = ["the nurse", "the patient"]
+    pronouns = ['he', 'she', 'they', 'her', 'him', 'his', 'them', 'their']
+    num_correct = 0
+
+    for example in winogender_dataset:
+        for word in example['sentence'].split(" "):
+            if word in pronouns:
+                pronoun = word
+                break
+        context = f"{example['sentence']} \"{pronoun}\" refers to:"
+        # sentid example - administrator.someone.1.male.txt
+        sentid_list = example['sentid'].split('.')
+        occupation = sentid_list[0]
+        participant = sentid_list[1]
+        correct_answer = int(sentid_list[2])
+        options = [occupation, participant]
+        winogender_probs = []
+
+        for option in options:
+            input_text = context + option
+            input_ids = tokenizer.encode(input_text, return_tensors='pt')
+            with torch.no_grad():
+                outputs = model(input_ids)
+                logits = outputs.logits
+                probs = logits.softmax(dim=-1)
+                option_ids = tokenizer.encode(option, return_tensors='pt')[0].tolist()
+                option_len = len(option_ids)
+                # take care of multi-tokens
+                seq_len = probs.shape[1]
+                option_probs = [probs[0, seq_len-option_len+i, id].item() for i, id in enumerate(option_ids)]
+                option_prob = np.prod(option_probs)
+                winogender_probs.append(option_prob)
+
+        if winogender_probs[0] > winogender_probs[1]:
+            predicted_answer = 0
+        else:
+            predicted_answer = 1
+
+        if predicted_answer == correct_answer:
+            num_correct = num_correct + 1
+
+    print("Accuracy of the model {args.model_name} on the winogender dataset is {num_correct/len(winogender_dataset)}")
 
 
 if __name__ == "__main__":
